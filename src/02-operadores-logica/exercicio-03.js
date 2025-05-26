fc9b80c361071d529beab3f53b6776a2c1bcd47e
Polyane Tuag <pollyportugues@gmail.com>
2025-05-26 19:43:35 -0300

    feat: add input validation and grading logic to avaliarSituacaoAluno function

--- src/02-operadores-logica/exercicio-03.js
+++ src/02-operadores-logica/exercicio-03.js
@@ -12,4 +12,35 @@
  */
 export function avaliarSituacaoAluno(nota1, nota2, nota3) {
   // Implementação da função
+  if (
+    typeof nota1 !== "number" ||
+    typeof nota2 !== "number" ||
+    typeof nota3 !== "number"
+  ) {
+    return "Por favor, forneça notas válidas";
+  }
+
+  // verifica o intervalo das notas válidas
+  if (nota1 < 0 || nota1 > 10) {
+    return "As notas devem estar entre 0 e 10";
+  }
+  if (nota2 < 0 || nota2 > 10) {
+    return "As notas devem estar entre 0 e 10";
+  }
+  if (nota3 < 0 || nota3 > 10) {
+    return "As notas devem estar entre 0 e 10";
+  }
+
+  // media das notas
+  const media = (nota1 + nota2 + nota3) / 3;
+
+  if (media < 5) {
+    return "Reprovado";
+  }
+
+  if (media < 7) {
+    return "Recuperação";
+  }
+
+  return "Aprovado";
 }
