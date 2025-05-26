90fb5d4a51405df0ea7274a73d576519e056a7dd
Polyane Tuag <pollyportugues@gmail.com>
2025-05-26 19:43:28 -0300

    feat: implement calcularDesconto function to calculate discounts based on purchase value and customer status

--- src/02-operadores-logica/exercicio-02.js
+++ src/02-operadores-logica/exercicio-02.js
@@ -4,12 +4,29 @@
  * - Compras > 1000: 10% desconto
  * - Cliente Premium: +5% desconto adicional
  * - Black Friday: +8% desconto adicional
- * @param {number} valorCompra 
- * @param {boolean} clientePremium 
- * @param {boolean} blackFriday 
+ * @param {number} valorCompra
+ * @param {boolean} clientePremium
+ * @param {boolean} blackFriday
  * @returns {number} Valor final com desconto
  */
 export function calcularDesconto(valorCompra, clientePremium, blackFriday) {
-  // Implementar aqui
-  return 0;
+  // sem desconto
+  let desconto = 0;
+
+  // Compras > 1000: 10% desconto
+  if (valorCompra > 1000) {
+    desconto = desconto + 0.1;
+  }
+
+  // Cliente Premium: +5% desconto adicional
+  if (clientePremium) {
+    desconto = desconto + 0.05;
+  }
+
+  // Black Friday: +8% desconto adicional
+  if (blackFriday) {
+    desconto = desconto + 0.08;
+  }
+
+  return valorCompra - valorCompra * desconto;
 }
