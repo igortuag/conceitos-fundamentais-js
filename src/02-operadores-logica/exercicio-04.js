f3c37db7836095c7354361301236e15af841ef19
Polyane Tuag <pollyportugues@gmail.com>
2025-05-26 19:57:23 -0300

    feat: add input validation for distancia in calcularPrecoPassagem function

--- src/02-operadores-logica/exercicio-04.js
+++ src/02-operadores-logica/exercicio-04.js
@@ -11,12 +11,24 @@ export function calcularPrecoPassagem(distancia) {
   // verifique se a entrada é um número, se não for, retorne uma mensagem de erro
   // 'Por favor, forneça uma distância válida'
 
+  if (typeof distancia !== "number") {
+    return "Por favor, forneça uma distância válida";
+  }
+
   // verifique se a distância é maior que zero, se não for, retorne uma mensagem de erro
   // "A distância deve ser maior que zero"
+  if (distancia < 0) {
+    return "A distância deve ser maior que zero";
+  }
 
   // Verifique o valor da distância e calcule o preço de acordo com as regras
+  if (distancia <= 200) {
+    const total = distancia * 0.5;
+    return `Preço da passagem: R$ ${total.toFixed(2)}`;
+  }
 
   // retorne o preço formatado com duas casas decimais
   // Exemplo: "Preço da passagem: R$ 50.00", utilize o método toFixed(2) para formatar o valor
-  return ``;
+  const total = distancia * 0.45;
+  return `Preço da passagem: R$ ${total.toFixed(2)}`;
 }
