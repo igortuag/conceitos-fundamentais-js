4975a77f244ff0f7b1316897e381424e7f20bbff
Polyane Tuag <pollyportugues@gmail.com>
2025-05-26 21:11:35 -0300

    feat: implement calcularAumentoSalario function with input validation and salary increase logic

--- src/02-operadores-logica/exercicio-06.js
+++ src/02-operadores-logica/exercicio-06.js
@@ -8,5 +8,26 @@
  * @returns {string} Mensagem com o novo salário e o aumento
  */
 export function calcularAumentoSalario(salario) {
-  return "";
+  if (typeof salario !== "number") {
+    return "Por favor, forneça um salário válido";
+  }
+
+  if (salario < 0) {
+    return "O salário deve ser maior que zero";
+  }
+
+  let aumento = 0.1;
+
+  if (salario <= 1250) {
+    aumento = 0.15;
+  }
+
+  let valorAumento = salario * aumento;
+  return `Salário anterior: R$ ${salario.toFixed(2)} Percentual de aumento: ${
+    aumento * 100
+  }% Valor do aumento: R$ ${valorAumento.toFixed(2)} Novo salário: R$ ${(
+    salario + valorAumento
+  ).toFixed(2)} `;
 }
+
+console.log(calcularAumentoSalario(1000));
