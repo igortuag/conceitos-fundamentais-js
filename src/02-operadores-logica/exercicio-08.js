0e333096802a1d6d14618f2ae31f92d123598b27
Polyane Tuag <pollyportugues@gmail.com>
2025-05-27 19:23:09 -0300

    feat: enhance number analysis function with proper validation and calculation logic

--- src/02-operadores-logica/exercicio-08.js
+++ src/02-operadores-logica/exercicio-08.js
@@ -5,7 +5,7 @@
  * - Identificar o maior, o menor e o valor do meio
  * - Indicar se há números iguais
  * - Calcular a média dos três números
- * 
+ *
  * @param {number} n1 - Primeiro número
  * @param {number} n2 - Segundo número
  * @param {number} n3 - Terceiro número
@@ -13,13 +13,52 @@
  */
 export function analisarNumeros(n1, n2, n3) {
   // você deve verificar se os números são válidos
+  if (
+    typeof n1 !== "number" ||
+    typeof n2 !== "number" ||
+    typeof n3 !== "number"
+  ) {
+    return "Por favor, forneça apenas números";
+  }
+
+  let maior = 0;
+  let menor = 0;
+  let temNumerosIguais = false;
+  const media = ((n1 + n2 + n3) / 3).toFixed(2);
 
   // você deve verificar qual é o maior número, qual é o menor, qual é a media e se há números iguais
+  // MAIOR
+  if (n1 > n2) {
+    maior = n1;
+  }
+
+  if (n3 > maior) {
+    maior = n3;
+  }
+  // MENOR
+  if (n1 < n2) {
+    menor = n1;
+  }
+
+  if (n2 < n3) {
+    menor = n2;
+  }
+
+  if (n3 < menor) {
+    menor = n3;
+  }
+
+  // SE HÁ IGUAIS
+  if (n1 === n2 || n2 === n3 || n1 === n3) {
+    temNumerosIguais = true;
+  }
 
   return {
-    maior: 0,
-    menor: 0,
-    media: 0,
-    temNumerosIguais: false
+    maior,
+    menor,
+    media,
+    temNumerosIguais,
   };
 }
+
+console.log("analisar>>>", analisarNumeros(10, 5, 8));
