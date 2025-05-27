fc05dd47dc12da88faf8f59dd7849c01e4401848
Polyane Tuag <pollyportugues@gmail.com>
2025-05-27 20:48:59 -0300

    feat: add input validation for encontrarMaior and encontrarMenor functions

--- src/02-operadores-logica/exercicio-13.js
+++ src/02-operadores-logica/exercicio-13.js
@@ -1,6 +1,6 @@
 /**
  * Exercício 13: Encontrar o Maior Número
- * 
+ *
  * @param {number} n1 - Primeiro número
  * @param {number} n2 - Segundo número
  * @param {number} n3 - Terceiro número
@@ -13,4 +13,24 @@
  */
 export function encontrarMaior(n1, n2, n3) {
   // Implementar aqui
+  if (
+    typeof n1 !== "number" ||
+    typeof n2 !== "number" ||
+    typeof n3 !== "number"
+  ) {
+    return "Por favor, forneça números válidos";
+  }
+
+  let maior = 0;
+
+  if (n1 >= n2) {
+    maior = n1;
+  }
+  if (n2 >= maior) {
+    maior = n2;
+  }
+  if (n3 >= maior) {
+    maior = n3;
+  }
+  return `O maior número é ${maior}`;
 }
--- src/02-operadores-logica/exercicio-14.js
+++ src/02-operadores-logica/exercicio-14.js
@@ -1,6 +1,6 @@
 /**
  * Exercício 14: Encontrar o Menor Número
- * 
+ *
  * @param {number} n1 - Primeiro número
  * @param {number} n2 - Segundo número
  * @param {number} n3 - Terceiro número
@@ -14,4 +14,24 @@
  */
 export function encontrarMenor(n1, n2, n3) {
   // Implementar aqui
+  if (
+    typeof n1 !== "number" ||
+    typeof n2 !== "number" ||
+    typeof n3 !== "number"
+  ) {
+    return "Por favor, forneça números válidos";
+  }
+
+  let r = n1;
+
+  if (n2 < r) {
+    r = n2;
+  }
+  if (n3 < r) {
+    r = n3;
+  }
+
+  return `O menor número é ${r}`;
 }
+
+console.log(("encontrarMenor(n1, n2, n3)", encontrarMenor(1, 2, 3)));
