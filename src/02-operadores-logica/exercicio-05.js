c7604bb14212b4428f6de5f8aad611880104cf90
Polyane Tuag <pollyportugues@gmail.com>
2025-05-26 21:11:29 -0300

    feat: implement verificarAnoBissexto function with input validation

--- src/02-operadores-logica/exercicio-05.js
+++ src/02-operadores-logica/exercicio-05.js
@@ -5,8 +5,22 @@
  * - Não divisível por 100 OU
  * - Divisível por 400
  * @param {number} ano - Ano a ser verificado
- * @returns {string} Mensagem indicando se é bissexto ou não eg. "2000 é um ano BISSEXTO" ou "2021 NÃO é um ano BISSEXTO", 
+ * @returns {string} Mensagem indicando se é bissexto ou não eg. "2000 é um ano BISSEXTO" ou "2021 NÃO é um ano BISSEXTO",
  */
 export function verificarAnoBissexto(ano) {
-  return "";
+  if (typeof ano !== "number") {
+    return "Por favor, forneça um ano válido";
+  }
+
+  if (ano < 0) {
+    return "O ano deve ser maior que zero";
+  }
+
+  if (ano % 4 == 0) {
+    if (ano % 100 != 0 || ano % 400 === 0) {
+      return `${ano} é um ano BISSEXTO`;
+    }
+  }
+
+  return `${ano} NÃO é um ano BISSEXTO`;
 }
