4c321ef54a5143f4d3585e393100fd029edde77a
Polyane Tuag <pollyportugues@gmail.com>
2025-05-28 19:41:09 -0300

    feat: implement validation and return array of even numbers in encontrarPares function

--- src/03-estruturas-controle/exercicio-01.js
+++ src/03-estruturas-controle/exercicio-01.js
@@ -12,11 +12,28 @@
  */
 export function encontrarPares(inicio, fim) {
   // verifique se os dados fornecidos são números, caso controario retorne  throw new Error('Por favor, forneça números válidos');
+  if (typeof inicio !== "number" || typeof fim !== "number") {
+    throw new Error("Por favor, forneça números válidos");
+  }
+
+  if (inicio > fim) {
+    throw new Error("O início deve ser menor que o fim");
+  }
+
+  const pares = [];
+
+  for (let i = inicio; i <= fim; i++) {
+    if (i % 2 == 0) {
+      pares.push(i);
+    }
+  }
 
   // Validação de intervalo
   // Verique se o fim é maior que o início, caso contrário retorne um erro  throw new Error("O início deve ser menor que o fim");
 
   // Usando white crie um array para armazenar os números pares
 
-  return [];
+  return pares;
 }
+
+// console.log("encontrarPares(inicio, fim)", encontrarPares(2, 8));
